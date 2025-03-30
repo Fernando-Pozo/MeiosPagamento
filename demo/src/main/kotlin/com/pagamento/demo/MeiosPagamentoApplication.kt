8cc7d4816baea6ccee8d0aea8ea9112d8658cbe0
fernandomqs <fernandopozomqs@gmail.com>
2025-03-30 10:45:14 -0300

    inicio

---
+++ demo/src/main/kotlin/com/pagamento/demo/MeiosPagamentoApplication.kt
@@ -0,0 +1,11 @@
+package com.pagamento.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeiosPagamentoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeiosPagamentoApplication>(*args)
+}
---
+++ demo/src/test/kotlin/com/pagamento/demo/MeiosPagamentoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pagamento.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeiosPagamentoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
