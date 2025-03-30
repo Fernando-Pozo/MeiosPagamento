87df84899337636f9f2d98ba0df38957949649ec
fernandomqs <fernandopozomqs@gmail.com>
2025-03-30 17:44:01 -0300

    melhorias e dockerfile

---
+++ demo/src/main/kotlin/com/pagamento/demo/Repository/EfetivarPagamentoClient.kt
@@ -0,0 +1,29 @@
+package com.pagamento.demo.Repository
+
+import com.pagamento.demo.model.Pagamento
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@Component
+class EfetivarPagamentoClient {
+
+    private val webClient = WebClient.create("http://processador-pagamento:8081")
+
+    fun processarCartao(request: Pagamento) {
+        webClient.post()
+            .uri("/cartao")
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(Void::class.java)
+            .block()
+    }
+
+    fun processarBoleto(request: Pagamento) {
+        webClient.post()
+            .uri("/boleto")
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(Void::class.java)
+            .block()
+    }
+}
--- demo/src/main/kotlin/com/pagamento/demo/service/MeioPagamentoService.kt
+++ demo/src/main/kotlin/com/pagamento/demo/service/MeioPagamentoService.kt
@@ -1,11 +1,14 @@
 package com.pagamento.demo.service
 
+import com.pagamento.demo.Repository.EfetivarPagamentoClient
 import com.pagamento.demo.enums.PagamentoTipo
 import com.pagamento.demo.model.Pagamento
 import org.springframework.stereotype.Service
 
 @Service
-class MeioPagamentoService {
+class MeioPagamentoService(
+    private val efetivarPagamentoClient: EfetivarPagamentoClient
+) {
 
     fun processar(pagamento: Pagamento){
         when (pagamento.tipo) {
@@ -13,19 +16,13 @@ class MeioPagamentoService {
                 if (pagamento.dadosCartao == null) {
                     throw IllegalArgumentException("Dados do cartão são obrigatórios para pagamento com cartão.")
                 }
-//                // Converte o model para o objeto que a 2ª peça espera
-//                val requestCartao = pagamento.toCartaoRequest()
-//                // Chama o endpoint da 2ª peça para processar o pagamento com cartão
-//                segundaPecaClient.processarCartao(requestCartao)
+                efetivarPagamentoClient.processarCartao(pagamento)
             }
             PagamentoTipo.BOLETO -> {
                 if (pagamento.dadosBoleto == null) {
                     throw IllegalArgumentException("Dados do boleto são obrigatórios para pagamento com boleto.")
                 }
-//                // Converte o model para o objeto que a 2ª peça espera
-//                val requestBoleto = pagamento.toBoletoRequest()
-//                // Chama o endpoint da 2ª peça para processar o pagamento com boleto
-//                segundaPecaClient.processarBoleto(requestBoleto)
+                efetivarPagamentoClient.processarBoleto(pagamento)
             }
 
             null -> TODO()
